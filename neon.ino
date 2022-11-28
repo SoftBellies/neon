8556a0de8a35c6fef93d3b1c449dd2b19c872fd0
Nicolas76000 <nicolas.web3.0@gmail.com>
2022-11-28 22:16:33 +0100

    Indentation

--- neon.ino
+++ neon.ino
@@ -4,11 +4,10 @@ int led2 = 12;
 
 void setup() {
   pinMode (led1,OUTPUT);
-  pinMode (led2, OUTPUT);
+  pinMode (led2,OUTPUT);
 }
 
-void effect1()
-{
+void effect1() {
   int i,clign, both;
 
   clign = random(5,20);
@@ -29,14 +28,12 @@ void effect1()
 
 
 }
-void effect2()
-{
+void effect2() {
 
 
 }
 
-void effect3()
-{
+void effect3() {
   global_blink(10);
   global_blink(30);
   global_blink(50);
@@ -49,8 +46,7 @@ void effect3()
   
 }
   
-global_blink(int period)
-{
+global_blink(int period) {
   int i;
   for(i=0;i<10;i++){
     delay(period);
@@ -64,21 +60,21 @@ global_blink(int period)
 
 void loop() {
   int randEffect;
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,HIGH);
   delay(random(2000,5000));
 
   randEffect = random(1,3);
   switch (randEffect){
-      case 1:
-        effect1();
-        break;
-      case 2:
-        effect2();
-        break;
-      case 3:
-        effect3();
-        break;
+    case 1:
+      effect1();
+      break;
+    case 2:
+      effect2();
+      break;
+    case 3:
+      effect3();
+      break;
     default:
       effect1();
       break;
